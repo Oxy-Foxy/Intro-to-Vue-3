cde039ab1a579dc739b3ddb47b48d1a14ec3c19f
OxyGrimm <oxy.grimm@kiv.dev>
2025-01-17 08:19:44 +0200

    added markup, added image and link with binding props

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'A pair of cozy socks'
+      image: './assets/images/socks_green.jpg',
+      url: '#'
     }
   }
 })
