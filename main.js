5f646790808b816e622486b048544d877e455e58
OxyGrimm <oxy.grimm@kiv.dev>
2025-01-21 07:52:49 +0200

    l6 - event handling

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234 , color: 'green'},
-        { id: 2235 , color: 'blue'},
-      ],
-      sizes: ['37', '38', '39', '40', '41', '42']
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+      ]
+    }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1
+      }
+    },
+    updateImage(src) {
+      this.image = src
     }
   }
 })
